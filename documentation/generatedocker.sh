8df463ff752104f792d3e6c7b4c8c662fb6dcc65
Justin Goro <justingoro@protonmail.com>
2021-07-11 10:03:54 +0200

    complete draft

--- documentation/generatedocker.sh
+++ documentation/generatedocker.sh
@@ -1,1 +1,1 @@
-docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` pandoc/latex --wrap=preserve --citeproc --bibliography=whitepaper.bib  Whitepaper.md -o behodler_whitepaper.pdf
+docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` pandoc/latex --wrap=preserve   Whitepaper.md -o behodler_whitepaper.pdf
